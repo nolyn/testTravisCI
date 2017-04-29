3a14c8db8380e2bfeee0b8a3bd6acffe93cbba3e
sauer <sauer@ient.rwth-aachen.de>
2017-04-29 12:50:37 +0200

    trying to fix travis

--- updateYUViewPKGBUILDVersion.sh
+++ updateYUViewPKGBUILDVersion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Cd to tmp"
 cd /tmp
-echo "Cloning aur pkg"
-git clone git+ssh://aur.archlinux.org/yuview-git.git
-cd yuview-git
+# echo "Cloning aur pkg"
+# git clone git+ssh://aur.archlinux.org/yuview-git.git
+# cd yuview-git
 
 # get the new version:
 echo "Cloning YUView"
@@ -13,12 +13,12 @@ new_version=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --
 echo ${new_version}
 cd ..
 
-echo "Updated version in files"
-sed -i "s/pkgver=.*$/pkgver=${new_version}/" PKGBUILD
-sed -i "s/pkgver = .*$/pkgver = ${new_version}/" .SRCINFO
+# echo "Updated version in files"
+# sed -i "s/pkgver=.*$/pkgver=${new_version}/" PKGBUILD
+# sed -i "s/pkgver = .*$/pkgver = ${new_version}/" .SRCINFO
 
-echo "commiting and pushing new version"
-git add -u
-git commit -m "updated package version"
-git push
+# echo "commiting and pushing new version"
+# git add -u
+# git commit -m "updated package version"
+# git push
 
